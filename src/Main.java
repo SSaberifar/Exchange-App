7cc8a4d3f792dda5a870784cbdad15370627a3eb
amirali-fadishei <amirali83.fadishei@gmail.com>
2024-04-27 21:32:36 +0330

    simple usuage of javafx scene builder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -12,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("graphic.fxml"));
         Scene scene = new Scene(root, Color.CHOCOLATE);
 
         stage.setTitle("Exchange App");
@@ -20,8 +21,8 @@ public class Main extends Application {
         Image icon = new Image("icon.png");
         stage.getIcons().add(icon);
 
-        stage.setWidth(1280);
-        stage.setHeight(720);
+        stage.setWidth(500);
+        stage.setHeight(500);
 
         stage.setScene(scene);
         stage.show();
