200bbafd164dafc8bc833dd00697111e38af1134
amirali-fadishei <amirali83.fadishei@gmail.com>
2024-05-11 16:59:36 +0330

    [fea]: user profile section complete,cursor add,...

--- src/main/java/ExchangeApp/Main.java
+++ src/main/java/ExchangeApp/Main.java
@@ -4,13 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Profile.fxml")));
         Scene loginPanel = new Scene(root);
         stage.setTitle("Fum coin exchange");
         stage.setScene(loginPanel);
--- src/main/java/ExchangeApp/Profile.java
+++ src/main/java/ExchangeApp/Profile.java
@@ -2,25 +2,100 @@ package ExchangeApp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import net.synedra.validatorfx.Validator;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Profile {
+
+    private final Validator validator = new Validator();
 
     @FXML
     Button menubtn;
     @FXML
     GridPane menu;
-    int sw=1;
+    @FXML
+    Button editbtn;
+    @FXML
+    TextField SignFName;
+    @FXML
+    TextField SignLName;
+    @FXML
+    TextField SignPhone;
+    @FXML
+    TextField SignEmail;
+    @FXML
+    PasswordField SignPass;
+    int menuSw=1;
+    int editSw=1;
 
-    public void menu(ActionEvent event){
-        if(sw==1){
-            menu.setVisible(false);
+    public void menu(){
+        menu.setVisible(menuSw != 1);
+        menuSw*=-1;
+    }
+    public void exit(ActionEvent event)throws IOException {
+        Parent root = new FXMLLoader(getClass().getResource("Login.fxml")).load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void editInfo(){
+        validator.createCheck().withMethod(c -> {
+            if (!Pattern.matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,20}$", c.get("password")) || c.get("password") == null) {
+                c.error("please enter valid password!");
+            }
+        }).dependsOn("password", SignPass.textProperty()).decorates(SignPass).immediate();
+        validator.createCheck().withMethod(c -> {
+            if (!Pattern.matches("^([\\u0600-\\u06FF\\s])+$", c.get("firstname")) || c.get("firstname") == null) {
+                c.error("please enter valid firstname!");
+            }
+        }).dependsOn("firstname", SignFName.textProperty()).decorates(SignFName).immediate();
+        validator.createCheck().withMethod(c -> {
+            if (!Pattern.matches("^([\\u0600-\\u06FF\\s])+$", c.get("lastname")) || c.get("lastname") == null) {
+                c.error("please enter valid lastname!");
+            }
+        }).dependsOn("lastname", SignLName.textProperty()).decorates(SignLName).immediate();
+        validator.createCheck().withMethod(c -> {
+            if (!Pattern.matches("[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+", c.get("email")) || c.get("email") == null) {
+                c.error("please enter valid email!");
+            }
+        }).dependsOn("email", SignEmail.textProperty()).decorates(SignEmail).immediate();
+        validator.createCheck().withMethod(c -> {
+            if (!Pattern.matches("^(?:(?:\\\\+?|00)(98)|(0))?((?:90|91|92|93|99)[0-9]{8})$", c.get("phone number")) || c.get("phone number") == null) {
+                c.error("please enter valid phone number!");
+            }
+        }).dependsOn("phone number", SignPhone.textProperty()).decorates(SignPhone).immediate();
+        if(editSw==1){
+            SignFName.setDisable(false);
+            SignLName.setDisable(false);
+            SignEmail.setDisable(false);
+            SignPass.setDisable(false);
+            SignPhone.setDisable(false);
+            editbtn.setText("ثبت اطلاعات کاربری");
         }else{
-            menu.setVisible(true);
+            if(validator.validate()){
+                SignFName.setDisable(true);
+                SignLName.setDisable(true);
+                SignEmail.setDisable(true);
+                SignPass.setDisable(true);
+                SignPhone.setDisable(true);
+                editbtn.setText("ویرایش اطلاعات کاربری");
+            }
         }
-        sw*=-1;
+        editSw*=-1;
     }
 
 }
